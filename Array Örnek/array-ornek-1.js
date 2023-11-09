fffbb83e0c1cb024e4d0441c64a0568aeabf2da4
mehmetkarakus <113180215+mehmetkarakus@users.noreply.github.com>
2023-11-09 08:52:08 +0300

    update

--- Array Örnek/array-ornek-1.js
+++ Array Örnek/array-ornek-1.js
@@ -1,6 +1,6 @@
 
 const name = ['m', 'e', 'h', 'm', 'e', 't'];
-const namefilter = name.filter((harf, index, self) => self.indexOf(harf) === index);
+const namefilter = name.filter((harf, index) => name.indexOf(harf) === index);
 
 document.getElementById("ornek-1").innerHTML = namefilter;
 
